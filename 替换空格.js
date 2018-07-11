6438509ce4ac0008dcebb47c95a386cc61467bff
Buddhas <2424932515@qq.com>
2018-07-11 09:15:54 +0800

    替换空格

--- 替换空格.js
+++ 替换空格.js
@@ -3,7 +3,7 @@ function replaceSpace(str)
      for(var i = 0; i < str.length; i++){
          if(str.charAt(i) == ' '){
             var arrStr = str.split("")
-            arrStr.splice(i,1,'%20')
+            arrStr.splice(i,1,'%20')//splice方法只在数组中有
             str = arrStr.join("")
          }
      }
