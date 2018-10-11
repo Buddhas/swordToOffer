d9e8fe2edf8528fe354182a86435d0f9fe2e6f46
123 <2424932515@qq.com>
2018-10-11 21:54:16 +0800

    输出旋转数组的最小

--- 输出旋转数组的最小元素.js
+++ 输出旋转数组的最小元素.js
@@ -1,8 +1,12 @@
 function minNumberInRotateArray(rotateArray)
 {
     rotateArray.sort((n,m)=>{
-        return n > m
+        return n - m
     })
-    console.log(rotateArray)
     
+    if(rotateArray.length == 0){
+        return 
+    }else{
+        return rotateArray[0]
+    }
 }
